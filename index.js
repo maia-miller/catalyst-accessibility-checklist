d963b2a41fd4418830831e9a64f7788da61872ae
Maia <32394837+maia-miller@users.noreply.github.com>
2020-10-26 17:24:58 +1300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Main from './pages/main/main';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>,
+   document.getElementById('root')
+);
