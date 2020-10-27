c0b0f08030d1110c76671e12b8dc10229f043ffc
maiamiller <maiamiller@catalyst.net.nz>
2020-10-27 16:39:54 +1300

    Add rest of elements to main Element accordion

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 
-import { Criterion, Subheading } from '../../assets/data/models';
-import SubheadingComponent from '../../components/subheading/subheading';
+import { Criterion, Subheading, Resource, CodeSample } from '../../assets/data/models';
 
 import './accordion.scss';
 
@@ -9,16 +8,19 @@ interface Props {
   accordionType: string,
   title: string,
   key: number,
-  criteria: Array<Criterion>,
-  subheadings?: Array<Subheading>
+  criteria?: Array<Criterion>,
+  subheadings?: Array<Subheading>,
+  resources?: Array<Resource>,
+  codeSamples?: Array<CodeSample>
+  code?: string
 }
 
 const Accordion = (props: Props) => {
-  const { accordionType, title, key, criteria, subheadings } = props
+  const { accordionType, title, key, criteria, subheadings, resources, codeSamples, code } = props
   const [isAccordionOpen, setIsAccordionOpen] = useState(false)
 
   return (
-    <div className={accordionType + ' accordion-wrapper'} key={key}>
+    <div className={accordionType + ' accordion-wrapper'} key={`${accordionType}-${key}`}>
 
       <div className="accordion-title" onClick={() => {setIsAccordionOpen(!isAccordionOpen)}}>
         {title}
@@ -29,7 +31,7 @@ const Accordion = (props: Props) => {
           <div className="accordion-body">
 
             <ul>
-              {criteria.map((criterion: Criterion, index: number) => {
+              {criteria && criteria.map((criterion: Criterion, index: number) => {
                 return (
                   <li key={index}>
                     {criterion.description} ({criterion.wcag})
@@ -52,6 +54,39 @@ const Accordion = (props: Props) => {
               )
             })}
 
+            {resources && resources.length > 0 &&
+              resources.map((resource) => {
+                return (
+                  <li>
+                    <a href={resource.link}>
+                      {resource.title}
+                    </a>
+                    {resource.description && (
+                      `: ${resource.description}`
+                    )}
+                  </li>
+                )
+              })
+            }
+
+            {codeSamples && codeSamples.length > 0 &&
+              codeSamples.map((codeSample: CodeSample, index: number) => {
+                return (
+                  <Accordion
+                    accordionType={'accordion-code'}
+                    title={codeSample.title}
+                    key={index}
+                    code={codeSample.code}
+                  />
+                )
+              })
+            }
+
+            {code &&
+              <div className="code">
+                {code}
+              </div>
+            }
           </div>
         }
       </div>
