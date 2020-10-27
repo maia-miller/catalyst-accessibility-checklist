d686d24ef905558a07d0870935a4ff9118dc38a3
maiamiller <maiamiller@catalyst.net.nz>
2020-10-27 16:17:15 +1300

    Create separate <Accordion /> component, which is called by <Checklist />

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-// import './main.css';
+import React, { useState } from 'react';
 
-const Accordion = () => {
+import { Criterion, Subheading } from '../../assets/data/models';
+import SubheadingComponent from '../../components/subheading/subheading';
+
+import './accordion.scss';
+
+interface Props {
+  title: string,
+  key: number,
+  criteria: Array<Criterion>,
+  subheadings?: Array<Subheading>
+}
+
+
+const Accordion = (props: Props) => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false)
+
   return (
-    <div className="Accordion">
-      <header className="Accordion-header">
-        <p>
-          Edit <code>src/Accordion.js</code> and save to reload.
-        </p>
-        <a
-          className="Accordion-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="accordion-wrapper" key={props.key}>
+      <div className="accordion-title" onClick={() => {setIsAccordionOpen(!isAccordionOpen)}}>
+        {props.title}
+      </div>
+      <div className="accordion-body-wrapper">
+        { isAccordionOpen &&
+          <div className="accordion-body">
+            <ul>
+              {props.criteria.map((criterion: Criterion, index: number) => {
+                return (
+                  <li key={index}>
+                    {criterion.description} ({criterion.wcag})
+                  </li>
+                )
+              })}
+            </ul>
+            {props.subheadings &&
+              props.subheadings.length > 0 &&
+              props.subheadings.map((subheading:
+              Subheading, index: number) => {
+              return (
+                <SubheadingComponent subheading={subheading} key={index} />
+              )
+            })}
+          </div>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default Accordion;
